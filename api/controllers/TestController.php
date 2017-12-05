b4de21c51d9a2080dcc6a58f65099bd7d67db149
stcar <1377079592@qq.com>
2017-12-05 22:42:13 +0800

    bug

--- api/controllers/TestController.php
+++ api/controllers/TestController.php
@@ -1,13 +1,20 @@
 <?php
 
-if(Yii::$app->wechat->isWechat && !Yii::$app->wechat->isAuthorized()) {
-  var_dump( Yii::$app->wechat->authorizeRequired()->send());
-}else{
-    echo 123;
+class SiteController extends Controller{
+
+    public function index(){
+        if(Yii::$app->wechat->isWechat && !Yii::$app->wechat->isAuthorized()) {
+            var_dump( Yii::$app->wechat->authorizeRequired()->send());
+          }else{
+              echo 123;
+          }
+        
+
+    }
+
+
+
 }
-
-
-
 
 
 
