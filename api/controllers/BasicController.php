d35bb72c2236005b6ea12e299cba927ee6874d5a
stcar <1377079592@qq.com>
2017-12-05 23:32:00 +0800

    basic

---
+++ api/controllers/BasicController.php
@@ -0,0 +1,21 @@
+<?php
+namespace app\controllers;
+use yii\web\Controller;
+use Yii;
+class BasicController extends Controller{
+
+    	/*构造方法：确认是否有openid*/
+	public function __construct($id,$module = null){
+        
+        parent::__construct($id,$module);
+
+        use EasyWeChat\Foundation\Application;
+
+        $app = new Application(Yii::params->wechat);
+               
+    }
+
+
+
+
+}
