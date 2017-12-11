92fc608005f3cff9caefe9b6a9ef1a2f75938e65
Ramroll Wei <ramrollwei@RamrolldeMacBook-Pro.local>
2017-12-12 00:26:11 +0800

    add 前赤壁赋

--- frontend/src/screen/page1.js
+++ frontend/src/screen/page1.js
@@ -33,8 +33,11 @@ export default class Page1 extends Component{
                     })}
                 </ul>
 
-                <div className="resp-box"                
-                >
+                <div className="text-block">
+                “客亦知夫水与月乎？逝者如斯，而未尝往也；盈虚者如彼，而卒莫消长也。盖将自其变者而观之，则天地曾不能以一瞬；自其不变者而观之，则物与我皆无尽也，而又何羡乎！且夫天地之间，物各有主，苟非吾之所有，虽一毫而莫取。惟江上之清风，与山间之明月，耳得之而为声，目遇之而成色，取之无禁，用之不竭，是造物者之无尽藏也，而吾与子之所共适。”
+                </div>
+                <div className="text-block">
+                客喜而笑，洗盏更酌。肴核既尽，杯盘狼藉。相与枕藉乎舟中，不知东方之既白。
                 </div>
 
                 <ButtonArea>
